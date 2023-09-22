5d896f83d95aa7ba05fb564a1d328ece4853c316
jeroenmeijaard <meijaard.j@gmail.com>
2023-09-22 14:02:50 +0000

    add

--- coffee_shop_jeroenmeij/models/marts/core/customers.sql
+++ coffee_shop_jeroenmeij/models/marts/core/customers.sql
@@ -9,28 +9,34 @@
 
 {{ config(materialized='table') }}
 
-with customer_orders as (
+with customers as (
+    select * from {{ ref('stg_coffee_shop__customers') }}
+),
+
+orders as (
+    select * from {{ ref('stg_coffee_shop__orders') }}
+),
+ 
+
+customer_orders as (
   select
      customer_id
      , count(*) as n_orders
      , min(created_at) as first_order_at
-
-  from {{ source('coffee_shop', 'orders') }} as orders 
+  from orders 
   group by 1
+),
+
+final as (
+  select 
+    customers.customer_id
+    , customers.name
+    , customers.email
+    , coalesce(customer_orders.n_orders, 0) as n_orders
+    , customer_orders.first_order_at
+  from customers
+  left join  customer_orders
+    on  customers.customer_id  = customer_orders.customer_id 
 )
 
-select 
-  customers.id as customer_id
-  , customers.name
-  , customers.email
-  , coalesce(customer_orders.n_orders, 0) as n_orders
-  , customer_orders.first_order_at
-from {{ source('coffee_shop', 'customers') }} as customers
-left join  customer_orders
-  on  customers.id = customer_orders.customer_id 
-
-/*
-    Uncomment the line below to remove records with null `id` values
-*/
-
--- where id is not null
+select * from final
--- coffee_shop_jeroenmeij/models/marts/core/monthly_new_customers.sql
+++ coffee_shop_jeroenmeij/models/marts/core/monthly_new_customers.sql
@@ -1,9 +1,17 @@
 
 -- Use the `ref` function to select from other models
+with customers as (
+    select * from {{ ref('customers') }}
+),
 
-select
-    date_trunc(first_order_at, month) as signup_month,
-    count(*) as new_customers
+final as (
+    select
+        date_trunc(first_order_at, month) as signup_month,
+        count(*) as new_customers
 
-from {{ ref('customers') }}
-group by 1
+    from customers
+    group by 1
+)
+
+Select *
+from final
